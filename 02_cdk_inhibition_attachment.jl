fb65c9edad6c0661305a4012ec73986b5c3ea018
el-uhu <luki.hutter@gmail.com>
2015-07-23 14:05:38 +0100

    Plot MCC-formation and Attachment in TEV

--- 02_cdk_inhibition_attachment.jl
+++ 02_cdk_inhibition_attachment.jl
@@ -11,11 +11,12 @@ include("00c_helperfunctions.jl")
 #                                                                              #
 ################################################################################
 
-global total_time = 10000000
+global total_time = 2500000
 global inhib = 0.75
 
 M = tev("stochastic")
 M.spec["total"] = total_time
+M.spec["seed"] = rand(1:200000)
 
 
 ################################################################################
@@ -27,14 +28,7 @@ M.spec["total"] = total_time
 #run regular simulation to find the value for t corresponding to the desired value of "tr"
 runSimulation!(M, "time")
 
-t1 = find_t_for_tr(M, "time", 10)
-t2 = total_time - t1
-
-pars2 = deepcopy(M.pars)
-pars2["RO"] = calculateInhibition(0.75)
-continueSim!(M, t1, t2, "add RO after t1", pars2)
-
-M.pars["RO"] = calculateInhibition(0.75)
+M.pars["RO"] = calculateInhibition(0.65)
 runSimulation!(M, "RO from beginning")
 
 
@@ -45,21 +39,47 @@ runSimulation!(M, "RO from beginning")
 ################################################################################
 
 
-figure()
-simname = "RO from beginning"
-plot(M.sims[simname].D["tr"], get_aKT(M, simname, "uKTtot"), color = "#601a5b", alpha = 1, linestyle = "-", linewidth = 2, label = "CDK-Inhibition (25%)")
+figure(figsize = [15,6])
+ax1 = subplot2grid([2,1], [0,0], 1, 1)
+title("Simulated Attachment Profiles and Rate of MCC-formation in Cohesion-Depletion", fonts["subplt_title"])
+simname = "time"
+plot(M.sims[simname].D["tr"], M.sims[simname].D["MCCformation "], color = "#288805", alpha = 1, linestyle = "-", linewidth = 2, label = "no CDK-Inhibition")
+xlim(0,50)
+xlabel("time (min)", fonts["subplt_axis"])
 
-# simname = "add RO after t1"
-# plot(M.sims[simname].D["tr"], get_aKT(M, simname, "uKTtot"), color = "#431a60", alpha = 1, linestyle = "-", linewidth = 2, label = "CDK-Inhibition after 10 minutes")
+ylim(0,7000)
+yticks([0,1200, 2400, 3600, 4800, 6000])
+ylabel("MCC formation (min-1)", fonts["subplt_axis"])
 
-simname = "add RO after t1_1"
-plot(M.sims[simname].D["tr"], get_aKT(M, simname, "uKTtot"), color = "k", alpha = 0.5, linestyle = "-", linewidth = 2, label = "no CDK-Inhibition")
 
-xlim(0,60)
-title("Simulated Attachment Profiles in Cohesion-Depletion", fonts["subplt_title"])
+ax1_2 = ax1[:twinx]()
+plot(M.sims[simname].D["tr"], get_aKT(M, simname, "uKTtot"), color = "#601a5b", alpha = 1, linestyle = "-", linewidth = 2, label = "no CDK-Inhibition")
+xlim(0,50)
 xlabel("time (min)", fonts["subplt_axis"])
 ylim(0,110)
 yticks([0,20, 40, 60, 80, 100])
 ylabel("attachment (%)", fonts["subplt_axis"])
-legend(loc = 4)
+
+
+ax1 = subplot2grid([2,1], [1,0], 1, 1)
+simname = "RO from beginning"
+
+plot(M.sims[simname].D["tr"], M.sims[simname].D["MCCformation "], color = "#288805", alpha = 1, linestyle = "-", linewidth = 2, label = "CDK-Inhibition (25%)")
+xlim(0,50)
+xlabel("time (min)", fonts["subplt_axis"])
+
+ylim(0,7000)
+yticks([0,1200, 2400, 3600, 4800, 6000])
+ylabel("MCC formation (min-1)", fonts["subplt_axis"])
+
+ax1_2 = ax1[:twinx]()
+plot(M.sims[simname].D["tr"], get_aKT(M, simname, "uKTtot"), color = "#601a5b", alpha = 1, linestyle = "-", linewidth = 2, label = "CDK-Inhibition (25%)")
+xlim(0,50)
+xlabel("time (min)", fonts["subplt_axis"])
+ylim(0,110)
+yticks([0,20, 40, 60, 80, 100])
+ylabel("attachment (%)", fonts["subplt_axis"])
+
+
+subplots_adjust(left = 0.07, right = 0.93, hspace = 0.4, wspace = 0.10, top = 0.90, bottom = 0.15)
 savefig(pwd()*"/plots/inhibition_attachment.pdf")
